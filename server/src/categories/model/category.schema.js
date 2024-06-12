2e89ac3adb99805cd3871ed7b6175085f3cd4899
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2024-06-11 20:13:32 -0500

    change in the category model

--- server/src/categories/model/category.schema.js
+++ server/src/categories/model/category.schema.js
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 const categorySchema = new Schema({
-  _id: { type: String, uuidv4 },
+  _id: { type: String, default: uuidv4 },
   name: { type: String, required: true },
   parent: { type: String, ref: 'Category', default: null },
   children: [{ type: String, ref: 'Category' }]

3fbe9956c8f37fee75bb5175327889d6da93d524
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2024-06-15 13:26:15 -0500

    Add validation to user routes with zod dtos

--- server/src/users/routes/user.routes.js
+++ server/src/users/routes/user.routes.js
@@ -5,13 +5,18 @@ import updateEmail from '../controllers/updateEmail.controller.js';
 import updatePassword from '../controllers/updatePassword.controller.js';
 import deleteUser from '../controllers/deleteUser.controller.js';
 import authenticate from '../../auth/Middlewares/authenticate.middleware.js';
+import validateDto from '../../auth/Middlewares/validateDto.middleware.js';
+import updateUserDto from '../dto/updateUser.dto.js';
+import updateEmailDto from '../dto/updateEmail.dto.js';
+import updatePasswordDto from '../dto/updatePassword.dto.js';
+import deleteUserDto from '../dto/deleteUser.dto.js';
 
 const userRouter = Router();
 
 userRouter.get('/get-user/:id', authenticate, getUser);
-userRouter.put('/update-user/:id', authenticate, updateUser);
-userRouter.patch('/update-email/:id', authenticate, updateEmail);
-userRouter.patch('/update-password/:id', authenticate, updatePassword);
-userRouter.delete('/delete-user/:id', authenticate, deleteUser);
+userRouter.put('/update-user/:id', validateDto(updateUserDto), authenticate, updateUser);
+userRouter.patch('/update-email/:id', validateDto(updateEmailDto), authenticate, updateEmail);
+userRouter.patch('/update-password/:id', validateDto(updatePasswordDto), authenticate, updatePassword);
+userRouter.delete('/delete-user/:id', validateDto(deleteUserDto), authenticate, deleteUser);
 
 export default userRouter;

fef9feedd8a41eded0d8af9eebb2e6b6df127497
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2024-06-18 15:46:43 -0500

    Add basic header

---
+++ client/src/components/BasicHeader.jsx
@@ -0,0 +1,16 @@
+export default function BasicHeader() {
+  return (
+    <header className='bg-azul py-0 px-12 max-[678px]:px-4'>
+      <nav className='w-full max-w-[1400px] my-o mx-auto h-16 flex justify-between items-center'>
+        <figure>
+          <a
+            href='/'
+            className='text-blanco font-poppins text-2xl font-bold no-underline'
+          >
+            Hogarfy
+          </a>
+        </figure>
+      </nav>
+    </header>
+  );
+}

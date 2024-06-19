21be6893f1ff69d57ca92737f6a1daf952a06f02
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2024-06-18 21:20:47 -0500

    Add some styles to the tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,21 @@ export default {
     flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: 'hsl(var(--blue))',
+        white: 'hsl(var(--white))',
+        black: 'hsl(var(--black))',
+        dark: 'hsl(var(--dark))',
+        'light-blue': 'hsl(var(--light-blue))',
+        'light-gray': 'hsl(var(--light-gray))',
+        'dark-gray': 'hsl(var(--dark-gray))'
+      },
+      fontFamily: {
+        roboto: 'var(--roboto)',
+        poppins: 'var(--poppins)'
+      }
+    },
   },
   plugins: [
     flowbite.plugin(),

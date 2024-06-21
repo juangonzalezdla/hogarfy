3dbccab5f105f21a314f1ed77375173f34ac8440
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2024-06-21 15:28:11 -0500

    Add schemas for validate the forms

---
+++ client/src/schemas/deleteUser.schema.js
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const deleteUserSchema = z.object({
+  password: z
+    .string({ required_error: 'Contraseña es requerida' })
+    .min(10, { message: 'Mínimo 10 caracteres' })
+    .max(25, { message: 'Máximo 25 caracteres' })
+    .refine((value) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/.test(value),
+      { message: 'Contraseña inválida, Mínimo: Una mayúscula, una minúscula y un número' }
+    ),
+});
+
+export default deleteUserSchema;

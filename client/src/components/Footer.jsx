e7c9a0c75c5e8a84f7f51aaaf138b1d074f62be3
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2024-06-26 17:10:26 -0500

    Add footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import { Link } from 'react-router-dom';
+import Logo from './Logo';
+
+const legalInformation = [
+  { id: 1, name: '¿Quiénes somos?', url: '/legal/sobre-nosotros' },
+  { id: 2, name: 'Preguntas frecuentes', url: '/legal/preguntas-frecuentes' },
+  { id: 3, name: 'Términos y condiciones', url: '/legal/terminos-condiciones' },
+  { id: 4, name: 'Política de privacidad', url: '/legal/privacidad' },
+];
+
+const social = [
+  {
+    id: 1,
+    icon: 'bx bxl-facebook-circle bx-sm text-black group-hover:text-white',
+    url: 'https://www.facebook.com/',
+  },
+  {
+    id: 2,
+    icon: 'bx bxl-instagram bx-sm text-black group-hover:text-white',
+    url: 'https://www.instagram.com/',
+  },
+  {
+    id: 3,
+    icon: 'bx bxl-twitter bx-sm text-black group-hover:text-white',
+    url: 'https://www.twitter.com/',
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className='bg-dark text-white px-12 py-12 max-md:px-4'>
+      <div className='w-full max-w-[1400px] my-0 mx-auto flex justify-between max-md:flex-col max-md:gap-8'>
+        <section className='flex flex-col justify-center items-center'>
+          <Logo />
+          <span>© Copyright 2023, Hogarfy colombia SAS</span>
+        </section>
+
+        <section className='flex flex-col justify-center items-center'>
+          <h2 className='text-2xl font-bold mb-2'>Informacíon</h2>
+          {legalInformation.map((information) => (
+            <Link
+              to={information.url}
+              className='mb-1 hover:underline'
+              target='_blank'
+              key={information.id}
+            >
+              {information.name}
+            </Link>
+          ))}
+        </section>
+
+        <section className='flex flex-col items-center gap-2'>
+          <h2 className='text-2xl font-bold mb-2'>Siguenos</h2>
+          <div className='flex items-center gap-5'>
+            {social.map((social) => (
+              <a
+                href={social.url}
+                className='bg-white p-1.5 flex items-center rounded-lg border-2 border-solid border-white group hover:bg-dark'
+                target='_blank'
+                key={social.id}
+              >
+                <i className={social.icon}></i>
+              </a>
+            ))}
+          </div>
+        </section>
+      </div>
+    </footer>
+  );
+}
